4e18f73b255c7df7f0f204289b64b8b4b1b45d4a
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-04-08 03:14:17 +0400

    Add social media constants with corresponding icons and URLs

---
+++ src/lib/socials.const.tsx
@@ -0,0 +1,47 @@
+import {
+  FacebookSVG,
+  InstagramSVG,
+  LinkedInSVG,
+  DiscordSVG,
+  HuggingFaceSVG,
+  GitHubSVG,
+  TwitterSVG,
+} from "@/assets/socials.svgs";
+
+export const socialsConst = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com",
+    icon: <FacebookSVG />,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com",
+    icon: <InstagramSVG />,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com",
+    icon: <LinkedInSVG />,
+  },
+  {
+    name: "Discord",
+    url: "https://discord.com",
+    icon: <DiscordSVG />,
+  },
+  {
+    name: "Hugging Face",
+    url: "https://huggingface.co",
+    icon: <HuggingFaceSVG />,
+  },
+  {
+    name: "Github",
+    url: "https://github.com",
+    icon: <GitHubSVG />,
+  },
+  {
+    name: "Twitter",
+    url: "https://x.com",
+    icon: <TwitterSVG />,
+  },
+];
